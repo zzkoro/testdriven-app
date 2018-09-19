a58868864eb7317f71f5f252479e84baa9a91439
junemp <junemp@kh-ubuntu.hadoop.com>
2018-09-20 00:48:27 +0900

    first commit

---
+++ services/users/project/__init__.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from flask import Flask, jsonify
+
+
+# instantiate the app
+app = Flask(__name__)
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+print(app.config, file=sys.stderr)
+
+@app.route('/users/ping', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'pong!'
+    })
