edf05ac2bd23d12a8d76d8bbe017baffe049820f
junemp <junemp@gmail.com>
2018-09-23 12:39:19 +0900

    - 수정

--- services/users/project/api/users.py
+++ services/users/project/api/users.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from project import db
+from project.api.models import  User
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -9,3 +11,16 @@ def ping_pong():
         'message': 'pong!'
 
     })
+
+@users_blueprint.route('/users', methods=['POST'])
+def add_user():
+    post_data = request.get_json()
+    username = post_data.get('username')
+    email = post_data.get('email')
+    db.session.add(User(username=username, email=email))
+    db.session.commit()
+    response_object = {
+        'status': 'success',
+        'message': f'{email} was added!'
+    }
+    return jsonify(response_object), 201
