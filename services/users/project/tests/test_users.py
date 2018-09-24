8d975f946c464f447e07967e3dca15430657246f
junemp <junemp@gmail.com>
2018-09-24 13:40:09 +0900

    testcase 추가

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -134,6 +134,13 @@ class TestUserService(BaseTestCase):
             self.assertIn('fletcher@notreal.com', data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
+    def test_main_no_users(self):
+        """Ensure the main route behaves correctly when no users have been added to the database."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'All Users', response.data)
+        self.assertIn(b'<p>No users!</p>', response.data)
+
 
 
 
