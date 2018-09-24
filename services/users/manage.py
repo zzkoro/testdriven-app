62ce562b7afcb7cd32963b3a0fba1b4dab703569
junemp <junemp@gmail.com>
2018-09-24 18:22:16 +0900

    coverage 추가

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,4 +1,15 @@
 import unittest
+import coverage
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
 
 from flask.cli import FlaskGroup
 
@@ -7,6 +18,7 @@ from project.api.models import User
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+
 
 @cli.command()
 def recreate_db():
@@ -29,6 +41,22 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+@cli.command()
+def cov():
+    """Run the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('projects/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
 
 if __name__ == '__main__':
     cli()
