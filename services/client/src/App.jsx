8645e944b6e708f7b2b4a097b7effd04a3ec015b
junemp <junemp@gmail.com>
2018-10-03 18:42:28 +0900

    첫화면수정

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -118,19 +118,7 @@ class App extends Component {
 
                           <Switch>
                               <Route exact path="/" render={() => (
-                                  <div>
-                                      <h1 className="title is-1">All Users</h1>
-                                      <hr/><br/>
-
-                                      <AddUser
-                                          username={this.state.username}
-                                          email={this.state.email}
-                                          addUser={this.addUser}
-                                          handleChange={this.handleChange}
-                                      />
-                                      <br/><br/>
-                                      <UsersList users={this.state.users}/>
-                                  </div>
+                                  <UsersList users={this.state.users} />
                               )} />
                               <Route exact path="/about" component={About}/>
                               <Route exact path="/register" render={() => (
--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 const UsersList = (props) => {
     return (
         <div>
+            <h1 className="title is-1">All Users</h1>
+            <hr/><br/>
             {
                 props.users.map((user) => {
                     return (
-                        <h4 key={user.id} className="box title is-4">{user.username}</h4>
+                        <h4
+                            key={user.id}
+                            className="box title is-4"
+                        >{user.username}</h4>
                     )
                 })
             }
         </div>
     )
-
 };
 
 export default  UsersList;
